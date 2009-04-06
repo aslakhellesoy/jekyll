12e8ea143023fc540f58d957fc15fd2dfc3e4e2b
Nathan Agrin <n8agrin@gmail.com>
2009-04-05 21:55:47 -0700

    Added textile_include tag

--- lib/jekyll/tags/include.rb
+++ lib/jekyll/tags/include.rb
@@ -5,9 +5,13 @@ module Jekyll
       super
       @file = file.strip
     end
-
+    
+    def check_file(file)
+      return @file !~ /^[a-zA-Z0-9_\/\.-]+$/ || @file =~ /\.\// || @file =~ /\/\./
+    end
+    
     def render(context)
-      if @file !~ /^[a-zA-Z0-9_\/\.-]+$/ || @file =~ /\.\// || @file =~ /\/\./
+      if check_file(@file)
         return "Include file '#{@file}' contains invalid characters or sequences"
       end
 
@@ -25,7 +29,30 @@ module Jekyll
       end
     end
   end
+  
+  class TextileIncludeTag < IncludeTag
+    def render(context)
+      if check_file(@file)
+        return "Include file '#{@file}' contains invalid characters or sequences"
+      end
+
+      Dir.chdir(File.join(context.registers[:site].source, '_includes')) do
+        choices = Dir['**/*'].reject { |x| File.symlink?(x) }
+        if choices.include?(@file)
+          source = File.read(@file)
+          html = RedCloth.new(source).to_html
+          partial = Liquid::Template.parse(html)
+          context.stack do
+            partial.render(context)
+          end
+        else
+          "Included file '#{@file}' not found in _includes directory"
+        end
+      end
+    end
+  end
 
 end
 
 Liquid::Template.register_tag('include', Jekyll::IncludeTag)
+Liquid::Template.register_tag('textile_include', Jekyll::TextileIncludeTag)
