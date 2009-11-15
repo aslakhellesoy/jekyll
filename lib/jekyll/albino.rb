941613b29a7045870aeafc28d6402e4a9fb779f2
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-11-15 05:37:25 +0100

    Deal speacially with Ioke and C#

--- lib/jekyll/albino.rb
+++ lib/jekyll/albino.rb
@@ -85,6 +85,8 @@ class Albino
 
   def convert_options(options = {})
     @options.merge(options).inject('') do |string, (flag, value)|
+      value = "c#" if value == "cs" # Because Liquid strips away the '#' character
+      value = "text" if value == "ik" # Not supported yet
       string + " -#{flag} #{value}"
     end
   end
